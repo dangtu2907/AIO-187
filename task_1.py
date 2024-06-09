c966e588e7a4d37589541b3cc2cc0807dcd2c0d9
Đỗ Đăng Tú <tudodang75@gmail.com>
2024-06-09 21:15:21 +0700

    task_1 - classification model

--- task_1.py
+++ task_1.py
@@ -1,4 +1,4 @@
-def classification_model(tp, fp, fn):
+def evaluate_classification_model(tp, fp, fn):
     precision = tp / (tp + fp)
     recall = tp / (tp + fn)
     f1_score = 2 * ((precision * recall) / (precision * recall))
@@ -11,8 +11,11 @@ try:
     tp = int(input('Nhập tp: '))
     fp = int(input('Nhập fp: '))
     fn = int(input('Nhập fn: '))
+        
 except ValueError as e:
     print(e)
-    print('All inputs must be integers and greater than 0')
+    print('All inputs must be integers')
+if  tp <= 0 or fp <= 0 or fn <= 0:
+    print("tp, fp and fn must be greater than 0")
 else:
-    classification_model(tp, fp, fn) 
+    evaluate_classification_model(tp, fp, fn) 
